66c4580f1b4ba31020f3542e32a0f28d0ab4228b
Vishal Modi <13108358+vishalmodi@users.noreply.github.com>
2020-07-15 18:31:08 -0700

    Fix web elements of Search control

--- lyftdemo/lib/controls/search.py
+++ lyftdemo/lib/controls/search.py
@@ -1,5 +1,6 @@
 
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 
 class SearchControl:
@@ -11,15 +12,15 @@ class SearchControl:
 
     @property
     def get_parent_elem(self):
-        return self.driver.find_element_by_xpath(self.parent_loc)
+        return self.driver.find_element(By.XPATH, self.parent_loc)
 
     @property
     def search_button_elem(self):
-        return self.get_parent_elem.find_element_by_xpath(self.search_button_loc)
+        return self.get_parent_elem.find_element(By.XPATH, self.search_button_loc)
 
     @property
     def search_input_elem(self):
-        return self.driver.find_element_by_xpath(self.input_loc)
+        return self.driver.find_element(By.XPATH, self.input_loc)
 
     def search_for(self, value):
         self.search_input_elem.send_keys(value)
